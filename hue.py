7d2f8a574222fa8089f3efa7f64cedc980cd6615
GitAmirHub <aacthu2u@gmail.com>
2020-12-09 15:02:14 +0000

    Changed from web scraping to json queries.

--- hue.py
+++ hue.py
@@ -1,31 +1,55 @@
-from urllib.request import urlopen as uReq
-from bs4 import BeautifulSoup as soup
-import ssl
+#from urllib.request import urlopen as uReq
+#from bs4 import BeautifulSoup as soup
+#import ssl
+import requests
+from requests.models import Response
 
-discovery_url = 'https://discovery.meethue.com/' #used to get ip address of bridge incase it changes (dynamic)
+"""Old method was going to use web scraping to input the json commands and exectue, however I did not want to use Selenium to 
+click on the get/put buttons etc. Ended up using requests to request json queries which works well for simple hue commands."""
+
+""" discovery_url = 'https://discovery.meethue.com/' #used to get ip address of bridge incase it changes (dynamic)
 my_url = 'https://192.168.1.104/debug/clip.html' #url of bridge, currently. Will change to be automated from discovery_url later
 user = '7vZqfGXI17ej986DZw1azmHM2pEd9j4Boa5H3qdQ' #Registered user so does not need to authenticate every time
 
 uClient = uReq(my_url, context=ssl._create_unverified_context())
 page_html = uClient.read() #reads the page and stores it
 uClient.close() #closes the reader
-page_soup = soup(page_html, "html.parser") #creates a soup of the page in html
+page_soup = soup(page_html, "html.parser") #creates a soup of the page in html """
 
 
 #Gets the lights info and prints to terminal
 def getLightInfo():
-    #Modifies the input box to add the user for authentication
-    user_tag = page_soup.input
-    user_tag['value'] = '/api' + user
-    pass
+    headers = {
+        'Accept': 'application/json',
+    }
+    data = ''
+    response = requests.get('http://192.168.1.104/api/7vZqfGXI17ej986DZw1azmHM2pEd9j4Boa5H3qdQ/lights', headers=headers, data=data)
+    response.raise_for_status()
+    jsonResponse = response.json()
+    print(jsonResponse)
 
 #Turns the bedroom light on
 def lightOn():
-    pass
+    headers = {
+        'Accept': 'application/json',
+    }
+    data = '{"on": true,"bri": 254,"hue": 8402,"sat": 140,"effect": "none"}'
+    response = requests.put('http://192.168.1.104/api/7vZqfGXI17ej986DZw1azmHM2pEd9j4Boa5H3qdQ/lights/2/state', headers=headers, data=data)
+    response.raise_for_status()
+    jsonResponse = response.json()
+    print(jsonResponse)
+    
 
 #Turns the bedroom light off
 def lightOff():
-    pass
+    headers = {
+    'Accept': 'application/json',
+    }
+    data = '{"on":false}'
+    response = requests.put('http://192.168.1.104/api/7vZqfGXI17ej986DZw1azmHM2pEd9j4Boa5H3qdQ/lights/2/state', headers=headers, data=data)
+    response.raise_for_status()
+    jsonResponse = response.json()
+    print(jsonResponse)
 
 #Sets the bedroom light colour
 def setColour(rgb):
